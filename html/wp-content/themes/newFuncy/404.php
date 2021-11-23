f1b8c522c6e6f05614d330c515ecdf55fe2c63e7
AlejandroGonzalez02 <agonzalezalonso@danielcastelao.org>
2021-11-23 13:43:11 +0100

    añadido o 404

---
+++ html/wp-content/themes/newFuncy/404.php
@@ -0,0 +1,20 @@
+
+<div id="contenedor"class ="rojo">
+    <div id="cabecera">
+        <?php get_header(); ?>
+    </div>
+
+    <div id="cuerpo">
+       <img src="<?php echo get_stylesheet_directory_uri();?>:/image/juan.jpeg" id="imagen">
+        Juan, está triste...
+    </div>
+
+    <div id="texto"></div>
+
+    <div id="pie">
+        <?php get_footer(); ?>
+    </div>
+
+
+</div>
+
