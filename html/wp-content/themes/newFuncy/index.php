d77fa70d27c86325e5edc229af7b49ff2078019d
AlejandroGonzalez02 <agonzalezalonso@danielcastelao.org>
2022-02-23 14:01:46 +0100

    Loop no index

--- html/wp-content/themes/newFuncy/index.php
+++ html/wp-content/themes/newFuncy/index.php
@@ -1,25 +1,32 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <title>New Funcy</title>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_uri()?>">
-</head>
-<body>
+    <head>
+        <meta charset="utf-8">
+        <title>New Funcy</title>
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri()?>">
 
-<?php get_header();?>
+    </head>
+    <body>
+        <?php
+            define('WP_USE_THEMES', false);
+            get_header();
+        ?>
 
-<div id="cuerpo">
+        <div id="div_principal"
+            <p id="contenedor_imagen"><img src="<?php echo get_stylesheet_directory_uri();?>/Imagenes/Juan.jpeg" id="imagen"></p>
+            <br>
+            <h3 id="Subtitulos">Juan normal</h3>
+            <p id="parrafos"> Non ten nada que ver aquí. Este é un cabalo nomal, con moito estilo, niguén pode negar o estilo deste cabalo.</p>
+        </div>
 
-    <?php
-    echo get_stylesheet_uri();
-    echo "<h1 class='morado'>HOLA FUNCY</h1>";
+        <div id="div_principal2">
+            <p id="contenedor_imaxe"><img src="<?php echo get_stylesheet_directory_uri();?>:/Imagenes/JuanEstatua.jpg" id="imagen"></p>
+            <br>
+            <h3 id="Subtitulos">A estatua de Juan</h3>
+            <p id="parrafos">Unha estatua do noso queridisimo juan, considerado un deus en moitos lugares do mundo.</p>
+        </div>
 
-include("header.php");
-?>
-</div>
-
-</body>
+        <?php get_footer();?>
+    </body>
 </html>
 
-<?php get_footer();?>
